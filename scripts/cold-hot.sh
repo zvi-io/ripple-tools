a9248ac44c717c3183a6750df40f41585f185105
Jaka Hudoklin <jakahudoklin@gmail.com>
2016-11-21 23:27:55 +0100

    scripts/cold-hot: fix offline mode

--- scripts/cold-hot.sh
+++ scripts/cold-hot.sh
@@ -35,36 +35,37 @@ echo "* funding sequence: $FUNDING_SEQUENCE"
 echo "-> generating transactions"
 echo "* tx funding hot wallet"
 ripple-tools send-payment \
-	--amount 50 --to $COLD_ADDRESS --sequence $FUNDING_SEQUENCE  \
+	--amount 50 --to $COLD_ADDRESS \
+	--fee 0.000012 --sequence $FUNDING_SEQUENCE  \
 	--address $FUNDING_ADDRESS --secret $FUNDING_SECRET \
 	--format json --offline --export > transactions.json
 
 echo "* tx funding cold wallet"
 ripple-tools send-payment \
-	--amount 50 --to $HOT_ADDRESS --sequence $((FUNDING_SEQUENCE+1)) \
+	--amount 50 --to $HOT_ADDRESS \
+	--fee 0.000012 --sequence $((FUNDING_SEQUENCE+1)) \
 	--address $FUNDING_ADDRESS --secret $FUNDING_SECRET \
 	--format json --offline --export >> transactions.json
 
 echo "* tx setting flags on cold wallet"
 ripple-tools set-flag defaultRipple \
-    --address $COLD_ADDRESS \
-    --secret $COLD_SECRET \
-    --sequence 1 --format json --offline --export >> transactions.json
+    --address $COLD_ADDRESS --secret $COLD_SECRET \
+    --fee 0.000012 --sequence 1 \
+	--format json --offline --export >> transactions.json
 ripple-tools set-flag requireDestinationTag \
-    --address $COLD_ADDRESS \
-    --secret $COLD_SECRET \
-    --sequence 2 --format json --offline --export >> transactions.json
+    --address $COLD_ADDRESS --secret $COLD_SECRET \
+    --fee 0.000012 --sequence 2 \
+	--format json --offline --export >> transactions.json
 ripple-tools set-flag disallowIncomingXRP \
-    --address $COLD_ADDRESS \
-    --secret $COLD_SECRET \
-    --sequence 3 --format json --offline --export >> transactions.json
+    --address $COLD_ADDRESS --secret $COLD_SECRET \
+    --fee 0.000012 --sequence 3 \
+	--format json --offline --export >> transactions.json
 
 echo "* tx hot -> cold trust line"
 ripple-tools set-trust \
-    --address $HOT_ADDRESS \
-    --secret $HOT_SECRET \
-    --counterparty $COLD_ADDRESS \
-    --currency USD \
-    --sequence 1 --format json --offline --export >> transactions.json
+    --address $HOT_ADDRESS --secret $HOT_SECRET \
+    --counterparty $COLD_ADDRESS --currency USD \
+    --fee 0.000012 --sequence 1 \
+	--format json --offline --export >> transactions.json
 
 echo "-> done"
